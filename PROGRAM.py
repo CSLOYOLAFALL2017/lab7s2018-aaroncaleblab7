c8d8fb3a5d051c88854fca0a78ba245a2bb973fb
ccrenfroe <caleb1214@aol.com>
2018-03-22 22:50:21 -0400

    VERSION 2 Had to delete a line in the movies.csv file (was line 321) to make the program work. Came up with an error in reading the file type.

--- PROGRAM.py
+++ PROGRAM.py
@@ -11,31 +11,48 @@ def filename():
 # Parameters: file name of file to read from
 # Return: The maximum profit
 def maximum_profit (fileGiven):
-    maxProfit = 0
-    filename = open(fileGiven,"r")
-    for line in filename:
-        #print(line, end = "")
-        movieData = line
-        releaseDate, movieTitle, budget, boxOfficeGross = movieData.split(",")
-        # print(releaseDate)
-        # print(movieTitle)
-        # print(budget)
-        # print(boxOfficeGross)
-        budget = float(budget)
-        boxOfficeGross = float(boxOfficeGross)
-        profit = boxOfficeGross - budget
-        if profit > maxProfit:
-             maxProfit = profit
-             maxMovieTitle = movieTitle
-    print("The movie with the maximum profit is", maxMovieTitle , "with $", maxProfit,)
-    return(maxProfit)
+    try:
+        maxProfit = 0
+        filename = open(fileGiven,"r")
+        for line in filename:
+            #print(line, end = "")
+            movieData = line
+            releaseDate, movieTitle, budget, boxOfficeGross = movieData.split(",")
+            # print(releaseDate)
+            # print(movieTitle)
+            # print(budget)
+            # print(boxOfficeGross)
+            budget = float(budget)
+            boxOfficeGross = float(boxOfficeGross)
+            profit = boxOfficeGross - budget
+            if profit > maxProfit:
+                maxProfit = profit
+                maxMovieTitle = movieTitle
+        print("The movie with the maximum profit is", maxMovieTitle , "with $%.2f" % maxProfit,)
+        return(maxProfit)
+    except FileNotFoundError:
+        print("ERROR: FILE NOT FOUND")
 
 
 
 # Function's purpose: Process the file
 # Parameters: file name of file to read from, file name of file to write to
 # Return: None
-
+def writeNewFile(fileInput,fileOutput):
+    fileNew = open(fileOutput,"w")
+    fileOld = open(fileInput,"r")
+    for line in fileOld:
+        #print(line, end = "")
+            movieData = line
+            releaseDate, movieTitle, budget, boxOfficeGross = line.split(",")
+            print("Release Date:",releaseDate, file =fileNew)
+            print(movieTitle , file =fileNew)
+            print("Budge of Movie:", budget, file =fileNew)
+            print("Total Box Office Gross:", boxOfficeGross, file =fileNew)
+            budget = float(budget)
+            boxOfficeGross = float(boxOfficeGross)
+            profit = boxOfficeGross - budget
+    return
 
 
 
@@ -44,9 +61,10 @@ def maximum_profit (fileGiven):
 # Return: None
 def main():
     fileInput = filename()
+
+    print("Filename", fileInput,"exists")
     maximum_profit(fileInput)
-#
-# print("Filename", filename,"exists")
-#
+    fileOutput = input("Please enter the name of the file you want to output to >")
+    writeNewFile(fileInput,fileOutput)
 # output_filename = input("Please enter the output file name > ")
 main()
